01ca7a55ce1ba73beaf8b6fb5d061a0a8ce4b00f
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-11-16 21:11:10 +1100

    Make tests pass

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,8 +1,8 @@
 import { browser, element, by } from 'protractor';
 
-describe('QuickStart E2E Tests', function () {
+describe('Tour of Heros E2E Tests', function () {
 
-  let expectedMsg = 'Hello Angular';
+  let expectedMsg = 'Tour of Heroes';
 
   beforeEach(function () {
     browser.get('');
